b9643d43af7ebd13f64368f19ca133c5f0e3446a
bella <bella.royle@gmail.com>
2021-01-24 15:31:19 +0000

    corrected random num gen and started readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Meme What You Say
+
+## Table of Contents
+
+- [About](#About)
+- [How to Play](#How-to-play)
+- [Tech used](#Tech-used)
+- [Authors and acknowledgement](#Authors-and-acknowledgment)
+
+## About
+
+## How to Play
+
+## Tech Used
+
+## Authors and acknowledgement
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,7 +1,7 @@
 const randomCodeGen = () => {
   const randomChars = 'QWERTYUIOPASDFGHJKLZXCVBNM';
   let result = '';
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < 7; i++) {
     result += randomChars.charAt(
       Math.floor(Math.random() * randomChars.length)
     );
@@ -12,7 +12,7 @@ const randomCodeGen = () => {
 const randomNumberGen = (noOfRounds) => {
   let sequence = [];
   let availableNumbers = [];
-  for (let i = 1; i < 10; i++) {
+  for (let i = 1; i < 18; i++) {
     availableNumbers.push(i);
   }
   for (let i = 0; i < noOfRounds; i++) {
